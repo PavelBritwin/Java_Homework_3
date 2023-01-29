65d925fc8aff0d687ee787fd742686385e9657c6
Pavel <anohinpa@gmail.com>
2023-01-29 21:36:26 +0300

    Task1

--- Task1.java
+++ Task1.java
@@ -1,47 +1,28 @@
-package Java_homework_2;
-import java.io.IOException;
+// Пусть дан произвольный список целых чисел, удалить из него четные числа
+
+package Java_homework_3;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Random;
-import java.util.logging.FileHandler;
-import java.util.logging.Logger;
-import java.util.logging.SimpleFormatter;
 
 public class Task1 {
-    public static void main(String[] args) throws SecurityException, IOException {
-            Logger log = Logger.getLogger(Task1.class.getName());
-            FileHandler fh = new FileHandler("log.txt");
-            SimpleFormatter sf = new SimpleFormatter();
-            log.addHandler(fh);
-            fh.setFormatter(sf);
-            Random rnd = new Random();
-            String result = "";
-            int [] arr = new int[10];
-            for (int i = 0; i < 10; i++)
+    public static void main(String[] args) {
+        Random rnd = new Random();
+        List<Integer> l = new ArrayList<Integer>();
+        for (Integer item = 0; item < rnd.nextInt(10,15); item++)
+        {
+            l.add(rnd.nextInt(1,15));
+        }
+        System.out.println(l);
+        for (Iterator<Integer> i = l.iterator(); i.hasNext();)
+        {
+            if (i.next() % 2 == 0)
             {
-                arr[i] = rnd.nextInt(1, 11);
-                System.out.print(arr[i]+" ");
+                i.remove();
             }
-            System.out.println();
-            for (int j = 0; j < arr.length; j++)
-            {
-                int change = 0;
-                for (int k = 1; k < arr.length; k++)
-                {
-                    if (arr[k - 1] > arr[k])
-                    {
-                        change = arr[k - 1];
-                        arr[k - 1] = arr[k];
-                        arr[k] = change;
-                    }
-                }
-                for (int item : arr)
-                {
-                    result += item + " ";
-                }
-                log.info(result);
-                result = "";
-                if (change == 0){
-                    break;
-                }
-            }
+        }
+        System.out.println(l);
     }
 }
